86d51e4899a322438a0ef2d1a5b044ff0b9beeb9
Avish-code <avishved27@gmail.com>
2024-05-29 14:46:13 +0530

    first commit

---
+++ src/main/java/com/example/demo/BananaJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananaJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananaJenkinsApplication.class, args);
+	}
+
+}
